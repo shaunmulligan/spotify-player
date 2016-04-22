635c08e1a352e28f504819e91e709b24140b3bf3
Shaun Mulligan <switchflip.shaun@gmail.com>
2016-04-22 19:48:36 +0100

    add basic spotify script

--- app/server.js
+++ app/server.js
@@ -1,2 +1,20 @@
-
 console.log("starting node.js script...")
+
+var options = {
+    appkeyFile: '/data/spotify_appkeyFile.key',
+    cacheFolder: 'cache',
+    settingsFolder: 'settings'
+};
+var spotify = require('spotify')(options);
+
+var ready = function(err) {
+    if(err) {
+        console.log('Login failed', err);
+    } else {
+        console.log('node-spotify is ready to exeute more code!');
+        //your apps functionality should start here
+    }
+};
+spotify.on({
+    ready: ready
+};
