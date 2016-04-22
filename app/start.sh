b4e54b608d9a3b4f26ce1356eba3004df17d360e
Shaun Mulligan <switchflip.shaun@gmail.com>
2016-04-22 19:50:58 +0100

    correct ENV VAR name for APP_KEY

--- app/start.sh
+++ app/start.sh
@@ -3,7 +3,7 @@
 key=/data/spotify_appkey.key
 if [ ! -e "key" ]; then
   echo "creating spotify app key"
-  echo $THE_ENV_VAR | base64 -d > /data/spotify_appkey.key
+  echo $APP_KEY | base64 -d > /data/spotify_appkey.key
 else
     echo "Spotify Key already exists"
 fi
