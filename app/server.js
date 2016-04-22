6f76e79163ac5c5796c5ae156dd1ee09097b24b4
Shaun Mulligan <switchflip.shaun@gmail.com>
2016-04-22 20:11:18 +0100

    play a song on start

--- app/server.js
+++ app/server.js
@@ -1,11 +1,14 @@
 console.log("starting node.js script...")
 
+var username = process.env.SPOT_USER
+var pass = process.env.SPOT_PASS
+
 var options = {
-    appkeyFile: '/data/spotify_appkeyFile.key',
+    appkeyFile: '/data/spotify_appkey.key',
     cacheFolder: 'cache',
     settingsFolder: 'settings'
-};
-var spotify = require('spotify')(options);
+}
+var spotify = require('node-spotify')(options);
 
 var ready = function(err) {
     if(err) {
@@ -13,8 +16,13 @@ var ready = function(err) {
     } else {
         console.log('node-spotify is ready to exeute more code!');
         //your apps functionality should start here
+        var track = spotify.sessionUser.starredPlaylist.getTrack(1);
+        spotify.player.play(track);
     }
-};
+}
+
 spotify.on({
     ready: ready
-};
+})
+
+spotify.login(username, pass, false, false);
