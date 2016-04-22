ba16f7d1ad090cd3a1aaa9db3890939e1e5d7e17
Shaun Mulligan <switchflip.shaun@gmail.com>
2016-04-22 18:58:27 +0100

    initial commit

---
+++ app/start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+key=/data/spotify_appkey.key
+if [ ! -e "key" ]; then
+  echo "creating spotify app key"
+  echo $THE_ENV_VAR | base64 -d /data/spotify_appkey.key
+else
+    echo "Spotify Key already exists"
+fi
+
+node ./server.js
